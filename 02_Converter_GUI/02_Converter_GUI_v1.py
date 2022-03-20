da7d300ed925b7e2b33f1380a2e0fcbd1c075bb7
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-03-21 11:55:50 +1300

    Create 02_Converter_GUI_v1.py

---
+++ 02_Converter_GUI/02_Converter_GUI_v1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from functools import partial #To prevent unwanted windows
+import random
+
+class Converter:
+    def __init__(self):
+        # Formatting variables
+        background_color="light blue"
+

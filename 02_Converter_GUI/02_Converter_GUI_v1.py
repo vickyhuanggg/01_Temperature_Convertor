6442b5f82fa71109df99d6905f830deb18c4c846
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-03-22 12:43:49 +1300

    Update 02_Converter_GUI_v1.py

--- 02_Converter_GUI/02_Converter_GUI_v1.py
+++ 02_Converter_GUI/02_Converter_GUI_v1.py
@@ -1,9 +1,48 @@
 from tkinter import *
-from functools import partial #To prevent unwanted windows
+from functools import partial # To prevent unwanted windows
 import random
 
 class Converter:
     def __init__(self):
         # Formatting variables
         background_color="light blue"
+
+        # Converter Frame
+        self.converter_frame = Frame(width=300, bg=background_color,pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Converter Heading (row 0)
+        self.temp_heading_label = Label(self.converter_frame, text ="Temperature Converter", font = "Arial 16 bold", bg=background_color, padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
+
+        # User instructions (row1)
+        self.temp_instructions_label = Label(self.converter_frame, text="Type in the amount to be "
+                                                                        "converted and then push "
+                                                                        "one of the buttons below...",
+                                             font="Arial 10 italic", wrap=250,
+                                             justify=LEFT, bg=background_color, padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
+
+        # Temperature entry box(row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20, font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
+
+        #conversion buttons frame (row 3), orchid 3 | khaki1
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
+
+        self.to_c_button = Button(self.conversion_buttons_frame, text="To Centigrade", font="Arial 10 bold", bg="Khaki1", padx=10, pady=10)
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button=Button(self.conversion_buttons_frame, text="To Fahrenheit", font="Arial 10 bold", bg="Orchid1", padx=10, pady=10)
+        self.to_f_button.grid(row=0, column=1)
+
+        # Answer label (row 4)
+
+# #main routine
+if __name__ =="__main__":
+     root = Tk()
+     root.title("Temperature Converter")
+     something = Converter()
+     root.mainloop()
 

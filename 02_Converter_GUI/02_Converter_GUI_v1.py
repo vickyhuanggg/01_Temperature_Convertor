1d1b3f2c61e22fc89a153d6fbc1ef222be01cf2d
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-03-22 13:09:59 +1300

    Update 02_Converter_GUI_v1.py

--- 02_Converter_GUI/02_Converter_GUI_v1.py
+++ 02_Converter_GUI/02_Converter_GUI_v1.py
@@ -8,7 +8,7 @@ class Converter:
         background_color="light blue"
 
         # Converter Frame
-        self.converter_frame = Frame(width=300, bg=background_color,pady=10)
+        self.converter_frame = Frame(width=300, bg=background_color, pady=10)
         self.converter_frame.grid()
 
         # Temperature Converter Heading (row 0)
@@ -31,13 +31,25 @@ class Converter:
         self.conversion_buttons_frame = Frame(self.converter_frame)
         self.conversion_buttons_frame.grid(row=3, pady=10)
 
-        self.to_c_button = Button(self.conversion_buttons_frame, text="To Centigrade", font="Arial 10 bold", bg="Khaki1", padx=10, pady=10)
+        self.to_c_button = Button(self.conversion_buttons_frame, text="To Centigrade", font="Arial 10 bold", highlightbackground="Khaki1", padx=10, pady=10)
         self.to_c_button.grid(row=0, column=0)
 
-        self.to_f_button=Button(self.conversion_buttons_frame, text="To Fahrenheit", font="Arial 10 bold", bg="Orchid1", padx=10, pady=10)
+        self.to_f_button=Button(self.conversion_buttons_frame, text="To Fahrenheit", font="Arial 10 bold", highlightbackground="Orchid1", padx=10, pady=10)
         self.to_f_button.grid(row=0, column=1)
 
         # Answer label (row 4)
+        self.converted_label=Label(self.converter_frame, font="Arial 14 bold", fg="purple", highlightbackground=background_color, pady=10, text="Conversion goes here")
+        self.converted_label.grid(row=4)
+
+        # History / Help button frame (row 5)
+        self.hist_help_frame=Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist_button = Button(self.hist_help_frame, font="Arial 12 bold", text="Calculation History", width=15)
+        self.calc_hist_button.grid(row=0, column=0)
+
+        self.help_button=Button(self.hist_help_frame, font="Arial 12 bold", text="Help", width=5)
+        self.help_button.grid(row=0, column=1)
 
 # #main routine
 if __name__ =="__main__":

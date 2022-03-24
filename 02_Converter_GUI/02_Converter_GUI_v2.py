c5c9bd1f5ab911ff716383af194d64685168dd71
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-03-25 12:46:37 +1300

    Update 02_Converter_GUI_v2.py

--- 02_Converter_GUI/02_Converter_GUI_v2.py
+++ 02_Converter_GUI/02_Converter_GUI_v2.py
@@ -32,32 +32,32 @@ class Converter:
         self.conversion_buttons_frame.grid(row=3, pady=10)
 
         self.to_c_button = Button(self.conversion_buttons_frame, text="To Centigrade", font="Arial 10 bold",
-                                  highlightbackground="Khaki1", padx=10, pady=10,
-                                  command=lambda: self.temp_convert(-459))
+                                  bg="Khaki1", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-459), highlightbackground= background_color )
         self.to_c_button.grid(row=0, column=0)
 
         self.to_f_button=Button(self.conversion_buttons_frame, text="To Fahrenheit", font="Arial 10 bold",
-                                highlightbackground="Orchid1", padx=10, pady=10,
-                                command=lambda: self.temp_convert(-273))
+                                bg="Orchid1", padx=10, pady=10,
+                                command=lambda: self.temp_convert(-273), highlightbackground= background_color)
         self.to_f_button.grid(row=0, column=1)
 
         # Answer label (row 4)
-        self.converted_label=Label(self.converter_frame, font="Arial 14 bold", fg="purple", highlightbackground=background_color, pady=10, text="Conversion goes here")
+        self.converted_label=Label(self.converter_frame, font="Arial 14 bold", fg="purple", bg=background_color, pady=10, text="Conversion goes here")
         self.converted_label.grid(row=4)
 
         # History / Help button frame (row 5)
         self.hist_help_frame=Frame(self.converter_frame)
         self.hist_help_frame.grid(row=5, pady=10)
 
-        self.calc_hist_button = Button(self.hist_help_frame, font="Arial 12 bold", text="Calculation History", width=15)
+        self.calc_hist_button = Button(self.hist_help_frame, font="Arial 12 bold", text="Calculation History", highlightbackground =background_color, width=15)
         self.calc_hist_button.grid(row=0, column=0)
 
-        self.help_button=Button(self.hist_help_frame, font="Arial 12 bold", text="Help", width=5)
+        self.help_button = Button(self.hist_help_frame, font="Arial 12 bold", text="Help",  highlightbackground =background_color, width=5)
         self.help_button.grid(row=0, column=1)
 
     def temp_convert(self, low):
         print(low)
-        error = "#ffaff"
+        error = "#ffafaf"
         # Retrieve amount entered into Entry field
         to_convert = self.to_convert_entry.get()
 
@@ -73,10 +73,10 @@ class Converter:
                 answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
 
             elif low == -459 and to_convert >= low:
-                celsius = (to_convert - 32) +5/9
+                celsius = (to_convert - 32) * 5/9
                 to_convert = self.round_it(to_convert)
                 celsius = self.round_it(celsius)
-                answer = "{} degrees F is {} degrees c".format(to_convert, celsius)
+                answer = "{} degrees F is {} degrees C".format(to_convert, celsius)
 
             else:
                 # Input is invlaid (too cold)!!

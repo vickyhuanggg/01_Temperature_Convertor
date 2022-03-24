07f36ac012ea68ef15a23e3815807c17cd910fbc
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-03-25 09:49:10 +1300

    Update 02_Converter_GUI_v2.py

--- 02_Converter_GUI/02_Converter_GUI_v2.py
+++ 02_Converter_GUI/02_Converter_GUI_v2.py
@@ -55,18 +55,56 @@ class Converter:
         self.help_button=Button(self.hist_help_frame, font="Arial 12 bold", text="Help", width=5)
         self.help_button.grid(row=0, column=1)
 
-    def temp_convert(self, to):
-        print(to)
-
+    def temp_convert(self, low):
+        print(low)
+        error = "#ffaff"
         # Retrieve amount entered into Entry field
         to_convert = self.to_convert_entry.get()
 
         try:
             to_convert = float(to_convert)
-            print("yay")
+            has_errors = "no"
+
+            # check amount is a valid number
+            if low == -273 and to_convert >= low:
+                fahrenheit = (to_convert * 9/5) + 32
+                to_convert = self.round_it(to_convert)
+                fahrenheit = self.round_it(fahrenheit)
+                answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
+
+            elif low == -459 and to_convert >= low:
+                celsius = (to_convert - 32) +5/9
+                to_convert = self.round_it(to_convert)
+                celsius = self.round_it(celsius)
+                answer = "{} degrees F is {} degrees c".format(to_convert, celsius)
+
+            else:
+                # Input is invlaid (too cold)!!
+                answer = "Too cold!"
+                has_errors ="yes"
+
+            # Display answer
+            if has_errors == "no":
+                self.converted_label.configure(text=answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error)
+
+            # Add Answer to list for history
 
         except ValueError:
-            print("oops")
+            self.converted_label.configure(text="Enter a number!!",fg="red")
+            self.to_convert_entry.configure(bg=error)
+
+    # Round
+    def round_it(self, to_round):
+        if to_round % 1 ==0:
+            rounded = int(to_round)
+        else:
+            rounded = round(to_round, 1)
+
+        return rounded
 # #main routine
 if __name__ =="__main__":
      root = Tk()
